bde76c2324b155d4a6de8ec3649852a6b18d5003
James Day <jamesthomasday@gmail.com>
2018-01-29 19:38:30 +1100

    Adding documentation

--- R/elo_calculations.R
+++ R/elo_calculations.R
@@ -1,39 +1,60 @@
-map_outcome_to_margin <- function(margin, A = 0, K = 1, B = 0.05, v = 1, Q = 1, C = 1){
-  #Generalised logistic function is in format
-  #Y <- A + ((K-A) / ((C + (Q*exp(-B * X)))^(1/v)))
-  numer <- K-A #create numerator
-  denom <- C + (Q*exp(-B * margin)) #create denomenator
-  divis <- numer / denom^(1/v) #perform division
-  actual_outcome <- A + divis #add to A
-  return(actual_outcome)
-}
-
-
-
-find_expected_margin <- function(elo_difference, M = 400, B = 0.025){
-  #Traditinoal ELO equation for expected outcome
-  #format is ExpOutcome = 1/ 1+ 10^(ELO_Diff/M)
-  # X is ELO Diff
-  # M is scaling factor
-  expected_outcome <- find_expected_outcome(elo_difference, M = M)
-  
-  # Now run existing map_outcome_to_margin to outcome convert to margin
-  # Find expected (predicted) Margin
-  points <- -200:200
-  points_norm <- map_outcome_to_margin(points, B = B) #create vector of results
-  expected_margin <- points[which.min(abs(points_norm - expected_outcome))]
-  return(expected_margin)
-  
-}
-
+#' Find the expected probability given an ELO difference.
+#'
+#' \code{sum} returns the expected probability of winning based on an ELO points difference. 
+#'
+#' This is a generic ELO function in the form of prob = 1/1+10^(elo/M) 
+#' where elo is the elo difference between two teams and 
+#' M is the scaling factor for the elo difference (EXPAND)
+#'
+#' @param elo_difference Numeric Test
+#' @param M Test
+#' @return TEST
+#'
+#' @examples
+#' sum(1:10)
+#'
+#' \dontrun{
+#' sum("a")
+#' }
 find_expected_outcome <- function(elo_difference, M = 400){
-  #Traditinoal ELO equation for expected outcome
+  # Traditinoal ELO equation for expected outcome
   #format is ExpOutcome = 1/ 1+ 10^(ELO_Diff/M)
   # X is ELO Diff
   # M is scaling factor
   expected_outcome <- 1 / (1 + (10 ^(-elo_difference/M)))
   return(expected_outcome)
 }
+
+
+
+find_expected_margin <- function(elo_difference, M = 400, B = 0.025){
+  # Traditinoal ELO equation for expected outcome
+  # format is expected_outcome = 1/ 1+ 10^(elo_difference/M)
+  # X is ELO Diff
+  # M is scaling factor
+  expected_outcome <- find_expected_outcome(elo_difference, M = M)
+  
+  # Now we need to convert this to a margin. 
+  map_margin_to_prob <- function(margin, A = 0, K = 1, B = 0.05, v = 1, Q = 1, C = 1){
+    #Generalised logistic function is in format
+    #Y <- A + ((K-A) / ((C + (Q*exp(-B * X)))^(1/v)))
+    numer <- K-A #create numerator
+    denom <- C + (Q*exp(-B * margin)) #create denomenator
+    divis <- numer / denom^(1/v) #perform division
+    prob <- A + divis #add to A
+    return(prob)
+  }
+  
+  # Now run existing map_margin_to_prob to outcome convert to margin
+  # Find expected (predicted) Margin
+  points <- -200:200
+  points_norm <- map_margin_to_prob(points, M = M, B = B) # create vector of results
+  expected_margin <- points[which.min(abs(points_norm - expected_outcome))]
+  return(expected_margin)
+  
+}
+
+
 
 margin_of_victory_multiplier <- function(elo_difference, margin , J = 2.2){
   # performs a Margin of Victory Multiplier
