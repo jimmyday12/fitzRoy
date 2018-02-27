99ab5ff44abbdb4134fc1419a5ea8cafe34b88b6
James Day <james.day@fusionsport.com>
2018-02-27 13:32:32 +1100

    run both advanced/basic in loop

--- data-raw/scrape_footywire_basic.R
+++ data-raw/scrape_footywire_basic.R
@@ -5,42 +5,79 @@ library(tidyverse)
 library(rvest)
 
 
-# Create URL
-default.url <- "http://www.footywire.com/afl/footy/ft_match_statistics?mid="
-
 # Create function to do different years
-footywire_basic <- function(ids, type = "basic") {
+footywire_basic <- function(ids) {
   dat <- data.frame()
   for (i in seq_along(ids)) {
     
     ind <- ids[i]
     print(paste("Retreiving data from id", ind))
-    # Create URL
-    if(type == "basic"){
-      sel.url <- paste(default.url, ind, sep = "")
-    } else if(type == "advanced"){
-      sel.url <-     paste(default.url, i, "&advv=Y", sep="")
+    
+    get_table_data <- function(x){
+      # Check if URL exists
+      if(url.exists(x)){
+        htmlcode <- readLines(x)
+        
+        # Get Data
+        export.table <- readHTMLTable(htmlcode)
+        top.table <- as.data.frame(export.table[13])
+        bot.table <- as.data.frame(export.table[17])
+        
+        # Combine into one
+        ind.table <- rbind(top.table, bot.table)
+        
+        # Add match ID
+        ind.table <- ind.table %>% mutate(match_id = ind)
+        
+        # Clean up 
+        # Fix names
+        names(ind.table) <- names(ind.table) %>%
+          map_chr(function(x) str_replace(x, "NULL.", ""))
+        
+        ind.table <- as.data.frame(ind.table)
+        return(ind.table)
+      }
     }
     
-    # Check if URL exists
-    if(url.exists(sel.url)){
-    htmlcode <- readLines(sel.url)
-
-    # Get Data
-    export.table <- readHTMLTable(htmlcode)
-    top.table <- as.data.frame(export.table[13])
-    bot.table <- as.data.frame(export.table[17])
-
-    # Combine into one
-    ind.table <- rbind(top.table, bot.table)
-
-    # Add match ID
-    ind.table <- ind.table %>% mutate(match_id = ind)
-
+    # Create URL
+    default.url <- "http://www.footywire.com/afl/footy/ft_match_statistics?mid="
+    
+    # Create URLs
+    sel.url.basic <- paste(default.url, ind, sep = "")
+    sel.url.advanced <- paste(default.url, ind, "&advv=Y", sep="")
+    
+    # Do basic and advanced
+    ind.table.basic <- get_table_data(sel.url.basic)
+    ind.table.advanced <- get_table_data(sel.url.advanced)
+    
+    ind.table <- left_join(ind.table.basic, ind.table.advanced, by = "Player")
+    
+    # Get basic info
+    footywire <- read_html(sel.url) 
+    
+    game_details <- footywire %>% 
+      html_node("tr:nth-child(2) .lnorm") %>% 
+      html_text()
+    
+    home_team <- footywire %>%
+      html_node("#matchscoretable tr:nth-child(2) a") %>%
+      html_text()
+    
+    away_team <- footywire %>%
+      html_node("#matchscoretable tr~ tr+ tr a") %>%
+      html_text()
+    
+    game_date <- footywire %>% 
+      html_node(".lnormtop tr:nth-child(3) .lnorm") %>%
+      html_text()
+    
+    ## Add data to ind.table
+    ind.table <- ind.table %>%
+      mutate(home.team = home_team,
+             away.team = away_team)
+    
     # Bind to dataframe
     dat <- bind_rows(dat, ind.table)
-    }
-
     Sys.sleep(2)
     
   }
@@ -66,10 +103,7 @@ ptm <- proc.time() # set a time
 player_stats_basic <- footywire_basic(ids, type = "basic")
 proc.time() - ptm # return time
 
-# Clean up 
-# Fix names
-names(player_stats_basic) <- names(player_stats_basic) %>%
-  map_chr(function(x) str_replace(x, "NULL.", ""))
+
 
 # Write data using devtools
 devtools::use_data(player_stats_basic, overwrite = TRUE)
@@ -95,21 +129,4 @@ devtools::use_data(player_stats_advanced, overwrite = TRUE)
 # Home Team
 # Away Team
 
-footywire <- read_html(sel.url) 
 
-game_details <- footywire %>% 
-  html_node("tr:nth-child(2) .lnorm") %>% 
-  html_text()
-
-home_team <- footywire %>%
-html_node("#matchscoretable tr:nth-child(2) a") %>%
-  html_text()
-
-away_team <- footywire %>%
-  html_node("#matchscoretable tr~ tr+ tr a") %>%
-  html_text()
-
-game_date <- footywire %>% 
-  html_node(".lnormtop tr:nth-child(3) .lnorm") %>%
-  html_text()
-  
