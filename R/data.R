60bbfd4dbb7d637c8222643d1b0439fcb6930ebd
James Day <james.day@fusionsport.com>
2018-03-05 10:42:42 +1100

    adding data documentation

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Advanced player match statistics.
+#'
+#' A dataset containing the match player statitics from footywire.com for matches since 2010.
+#'
+#' @format A data frame with 71,104 rows and 42 variables:
+#' \describe{
+#'   \item{Date}{date of match, in yyyy-mm-dd format}
+#'   \item{Season}{season of match, in yyyy format}
+#'   \item{Round}{round of match}
+#'   ...
+#' }
+#' @source \url{http://www.footywire.com}
+"player_stats"
