63d7ba54f8d68e16c96becdf88ef5a2dbc1db0cf
James Day <james.day@fusionsport.com>
2018-03-12 09:45:04 +1100

    adding sim season internal function

--- R/elo_calculations.R
+++ R/elo_calculations.R
@@ -101,4 +101,36 @@ map_margin_to_outcome <- function(margin, A = 0, K = 1, B = 0.05, v = 1, Q = 1 ,
 }
 
 
-
+simSeason <- function(fixture, team_elo = data.frame(), simulation = 1) {
+  
+  # initialise data frame
+  simulated_results <- tibble()
+  
+  # Step through each game
+  for (i in seq_along(fixture$Game)) {
+    
+    # get game details
+    game <- fixture[i, ]
+    
+    # Find elo diff
+    game$home_elo <- team_elo$ELO[(team_elo$Team == game$Home)]
+    game$away_elo <- team_elo$ELO[(team_elo$Team == game$Away)]
+    elo_diff <- game$home_elo - game$away_elo + HGA
+    
+    # Find expected outcome based on elo
+    exp_margin <- find_expected_margin(elo_diff, M)
+    
+    # sample from rnorm of mean marg and historical SD
+    game$margin <- round(rnorm(1, exp_margin, sd = 41))
+    
+    team_elo$ELO[(team_elo$Team == game$Home)] <- update_elo(game$margin, home_elo, away_elo)
+    team_elo$ELO[(team_elo$Team == game$Away)] <- update_elo(game$margin, home_elo, away_elo, returns = "away")
+    
+    simulated_results <- simulated_results %>%
+      bind_rows(game)
+  }
+  
+  simulated_results <- simulated_results %>%
+    mutate(sim_number = simulation)
+  return(simulated_results)
+}
