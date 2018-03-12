7d8b0f50d354a450b12a10304ce88e97fffedf07
James Day <james.day@fusionsport.com>
2018-03-13 09:52:30 +1100

    changes to elo calculations

--- R/elo_calculations.R
+++ R/elo_calculations.R
@@ -1,15 +1,15 @@
 #' Find the expected outcome given an ELO difference.
 #'
-#' \code{find_expected_outcome} returns the expected outcome of winning based on an ELO points difference. 
+#' \code{find_expected_outcome} returns the expected outcome of winning based on an ELO points difference.
 #'
-#' This is a generic ELO function in the form of prob = 1/1+10^(elo/M) 
-#' where elo is the elo difference between two teams and 
+#' This is a generic ELO function in the form of prob = 1/1+10^(elo/M)
+#' where elo is the elo difference between two teams and
 #' M is the scaling factor for the elo difference (EXPAND)
 #'
-#' @param elo_difference Difference in elo ratings between two teams. Can be a positive or negative number. 
+#' @param elo_difference Difference in elo ratings between two teams. Can be a positive or negative number.
 #' @param M Test
-#' @return A numeric value between 0 and 1, where values >0.5 indicate a win 
-#' while values of <0.5 indicate a loss. 
+#' @return A numeric value between 0 and 1, where values >0.5 indicate a win
+#' while values of <0.5 indicate a loss.
 #' A value of 0.5 indicates a draw
 #'
 #' @examples
@@ -21,43 +21,42 @@
 #' }
 #' @export
 
-find_expected_outcome <- function(elo_difference, M = 400){
+find_expected_outcome <- function(elo_difference, M = 400) {
   # Error checks
-  if(!is.numeric(elo_difference)) stop("elo_difference must be numeric")
-  
+  if (!is.numeric(elo_difference)) stop("elo_difference must be numeric")
+
   # Use traditional elo calculation
-  expected_outcome <- 1 / (1 + (10 ^(-elo_difference/M)))
+  expected_outcome <- 1 / (1 + (10 ^ (-elo_difference / M)))
   return(expected_outcome)
 }
 
 
 #' @export
-find_expected_margin <- function(elo_difference, M = 400, B = 0.025){
+find_expected_margin <- function(elo_difference, M = 400, B = 0.025) {
   # Traditinoal ELO equation for expected outcome
   # format is expected_outcome = 1/ 1+ 10^(elo_difference/M)
   # X is ELO Diff
   # M is scaling factor
   expected_outcome <- find_expected_outcome(elo_difference, M = M)
-  
+
   # Now run existing map_margin_to_prob to outcome convert to margin
   # Find expected (predicted) Margin
   points <- -200:200
   points_norm <- map_margin_to_outcome(points, B = B) # create vector of results
   expected_margin <- points[which.min(abs(points_norm - expected_outcome))]
   return(expected_margin)
-  
 }
 
-#' Find new season ELO score by applying carryover factor. 
+#' Find new season ELO score by applying carryover factor.
 #'
 #' \code{calculate_season_carryover} returns an ELO rating that is scaled towards the mean based on a carryover weight.
 #'
 #' INSERT DESCRIPTION
 #'
-#' @param elo An ELO rating, typically taken as the end of season value. 
+#' @param elo An ELO rating, typically taken as the end of season value.
 #' @param initial_team Rating given to the intial team. All values are scaled towards this value
-#' @param weight A weighting for how much to regress the score towards `initial_team`. 
-#' A value of 1 would not regress the ELO rating at all 
+#' @param weight A weighting for how much to regress the score towards `initial_team`.
+#' A value of 1 would not regress the ELO rating at all
 #' while a value of 0 would regress all the way to `initial_team`
 #' @return A numeric value indicating the new ELO rating
 #'
@@ -68,96 +67,150 @@ find_expected_margin <- function(elo_difference, M = 400, B = 0.025){
 #' \dontrun{
 #' calculate_season_carryover(1650, weight = -10)
 #' }
-calculate_season_carryover <- function(elo, initial_team = 1500, weight = 0.5){
+calculate_season_carryover <- function(elo, initial_team = 1500, weight = 0.5) {
   # error checks
-  if(!is.numeric(elo)) stop("elo must be numeric")
-  if(!is.numeric(weight)) stop("weight must be numeric")
-  if(!is.numeric(initial_team)) stop("initial_team must be numeric")
-  if(weight < 0) stop("carryover_weight must be positive")
-  if(weight > 1) stop("carryover_weight must be between 0 and 1, inclusive")
+  if (!is.numeric(elo)) stop("elo must be numeric")
+  if (!is.numeric(weight)) stop("weight must be numeric")
+  if (!is.numeric(initial_team)) stop("initial_team must be numeric")
+  if (weight < 0) stop("carryover_weight must be positive")
+  if (weight > 1) stop("carryover_weight must be between 0 and 1, inclusive")
 
-  
-  
+
+
   new_elo <- (weight * elo) + (initial_team * (1 - weight))
   new_elo <- as.integer(floor(new_elo))
   return(new_elo)
-  
 }
 
 
-map_margin_to_outcome <- function(margin, A = 0, K = 1, B = 0.025, v = 1, Q = 1 , C = 1){
-  #Generalised logistic function is in format
-  #Y <- A + ((K-A) / ((C + (Q*exp(-B * X)))^(1/v)))
-  numer <- K-A #create numerator
-  denom <- C + (Q*exp(-B * margin)) #create denomenator
-  divis <- numer / denom^(1/v) #perform division
-  actOut <- A + divis #add to A
+map_margin_to_outcome <- function(margin, A = 0, K = 1, B = 0.025, v = 1, Q = 1, C = 1) {
+  # Generalised logistic function is in format
+  # Y <- A + ((K-A) / ((C + (Q*exp(-B * X)))^(1/v)))
+  numer <- K - A # create numerator
+  denom <- C + (Q * exp(-B * margin)) # create denomenator
+  divis <- numer / denom ^ (1 / v) # perform division
+  actOut <- A + divis # add to A
   return(actOut)
 }
 
-calculate_MOV <- function(elo_diff, margin, J = 2.2){
-  #performs a Margin of Victory Multiplier
+calculate_MOV <- function(elo_diff, margin, J = 2.2) {
+  # performs a Margin of Victory Multiplier
   # this allows for scaling of new result depending on if fav won or not
-  
+
   # First find if Fav won if so, make ELO_Fav positive
   if ((elo_diff * margin) > 0) {
-    #then both are same sign, meaning fav won
+    # then both are same sign, meaning fav won
     elo_fav <- elo_diff
-  } else #otherwise, they are different so underdog won
+  } else # otherwise, they are different so underdog won
   {
     elo_fav <- -elo_diff
   }
-  mult <- J / ((0.001 * elo_diff) + J) #find multiplier
-  MOV <-  log(abs(margin) + 1) * mult #multiply by nat log of abs margin
-  return (MOV)
+  mult <- J / ((0.001 * elo_diff) + J) # find multiplier
+  MOV <- log(abs(margin) + 1) * mult # multiply by nat log of abs margin
+  return(MOV)
 }
 
-#' Simulate a season based on ELO
+
+#' Find a team's new ELO given their previous ELO, home ground advantage and actual result.
 #'
-#' \code{simulated_season} takes a fixture and simulates remaining games based upon the teams starting ELO. 
+#' \code{update_elo} returns a new ELO value for the specified team.
 #'
-#' INSERT DESCRIPTION
+#' This calculates an updated ELO based on the ELO rating of each team,
+#' the home ground advatnage assigned and the actual margin. It can return either the home team ELO, the away team ELO or both
 #'
-#' @param fixture A dataframe containing upcoming matches EXPAND 
-#' @param team_elo A dataframe with each teams current ELO ratings
-#' @param simulation An optional simulation ID number
-#' @param HGA Home ground advantage, in arbitrary units
-#' @param M Weighting factor for ELO calculations
-#' @param stdev Standard Deviation of results
-#' @return A dataframe of simulated results
+#' @param margin The actual margin of the match, in points, from the home team perspective. Positive numbers indicate a home team win, negative numbers indicate an away team win. A value of 0 indicates a draw.
+#' @param elo_diff The difference between home and away ELO scores, including home ground advantage
+#' @param returns The value to return as an output. Can be either the home team ELO, the away team ELO or both. Returning both will return a vector of two values with the home team ELO listed first.
+#' @param HGA Home ground advantage
+#' @return Returns the ELO of the team specified in `returns`
 #'
-#' @export
-simulate_season <- function(fixture, team_elo = data.frame(), simulation = 1,
-                            HGA = 20, M = 400, stdev = 41) {
-  
-  # initialise data frame
-  simulated_results <- tibble()
-  
+#' @examples
+#' update_elo(26, elo_diff = 50)
+update_elo <- function(margin, elo_diff, MOV = 1, k = 20,
+                       M = 400, B = 0.025) {
+
+  # Error checks
+
+  # Find the expected outcome
+  expected_outcome <- find_expected_outcome(elo_diff, M = M)
+
+  # First normalises actual Outcome between 0 and 1, slightly squashed so that
+  # there are diminishing gains at higher levels.
+  actual_outcome <- map_margin_to_outcome(margin)
+
+  # Expected outcome is for home team. Away team is the negative of it, since
+  # ELO is zero sum
+  elo_change <- round((k * MOV * (actual_outcome - expected_outcome)))
+
+  return(elo_change)
+}
+
+
+process_matches <- function(data, team_elo, type = "historical",
+                            stdev = 41, HGA = 35, k = 20, M = 400,
+                            B = 0.025, carryover_weight = 0.6,
+                            init_elo = 1500) {
+
+  # Start progress bar
+  pb <- progress_estimated(nrow(data))
+
+  # Initialise a data frame
+  processed_results <- tibble()
+
   # Step through each game
-  for (i in seq_along(fixture$Game)) {
-    
+  for (i in seq_along(data$Game)) {
+    pb$tick()$print() # update the progress bar (tick())
+
     # get game details
-    game <- fixture[i, ]
-    
-    # Find elo diff
-    game$home_elo <- team_elo$ELO[(team_elo$Team == game$Home)]
-    game$away_elo <- team_elo$ELO[(team_elo$Team == game$Away)]
-    elo_diff <- game$home_elo - game$away_elo + HGA
-    
+    game <- data[i, ]
+
+    # Get current elo
+    home_elo <- team_elo$ELO[(team_elo$Team == game$Home.Team)]
+    away_elo <- team_elo$ELO[(team_elo$Team == game$Away.Team)]
+
+    if (game$Round.Number == 1) {
+      home_elo <- calculate_season_carryover(home_elo, initial_team = init_elo, weight = carryover_weight)
+      away_elo <- calculate_season_carryover(away_elo, initial_team = init_elo, weight = carryover_weight)
+    }
+
+    # Calculate ELO Diff
+    elo_diff <- home_elo - away_elo + HGA
+
     # Find expected outcome based on elo
-    exp_margin <- find_expected_margin(elo_diff, M)
+    exp_margin <- find_expected_margin(elo_diff, M = M, B = B)
+    exp_outcome <- find_expected_outcome(elo_diff, M = M)
+
+    if (type == "Simulation") {
+      # sample from rnorm of mean marg and historical SD
+      game$margin <- round(rnorm(1, exp_margin, sd = stdev))
+    }
+
+    # Find MOV multiplier
+    MOV <- calculate_MOV(elo_diff, game$Margin)
+
+    # Calculate ELO change
+    elo_change <- update_elo(
+      game$Margin, elo_diff, MOV = MOV,
+      k = k, M = M, B = B
+    )
     
-    # sample from rnorm of mean marg and historical SD
-    game$margin <- round(rnorm(1, exp_margin, sd = 41))
-    
-    team_elo$ELO[(team_elo$Team == game$Home)] <- update_elo(game$margin, game$home_elo, game$away_elo)
-    team_elo$ELO[(team_elo$Team == game$Away)] <- update_elo(game$margin, game$home_elo, game$away_elo, returns = "away")
-    
-    simulated_results <- simulated_results %>%
+    new_home_elo <- home_elo + elo_change
+    new_away_elo <- away_elo - elo_change
+    team_elo$ELO[(team_elo$Team == game$Home.Team)] <- new_home_elo
+    team_elo$ELO[(team_elo$Team == game$Away.Team)] <- new_away_elo
+
+    # Add new data to df
+    game <- game %>%
+      mutate(
+        home_elo = home_elo,
+        away_elo = away_elo,
+        exp_margin = exp_margin,
+        exp_outcome = exp_outcome
+      )
+
+    # Bind to bigger data frame
+    processed_results <- processed_results %>%
       bind_rows(game)
   }
-  
-  simulated_results <- simulated_results %>%
-    mutate(sim_number = simulation)
-  return(simulated_results)
+  return(processed_results)
 }
--- R/elo_main.R
+++ R/elo_main.R
@@ -1,115 +1,68 @@
-#' Find a team's new ELO given their previous ELO, home ground advantage and actual result.
+#' X
 #'
-#' \code{update_elo} returns a new ELO value for the specified team.
+#' \code{process_historical_elo} x
 #'
-#' This calculates an updated ELO based on the ELO rating of each team,
-#' the home ground advatnage assigned and the actual margin. It can return either the home team ELO, the away team ELO or both
+#' INSERT DESCRIPTION
 #'
-#' @param elo_home The absolute ELO rating of the home team
-#' @param elo_away The absolute ELO rating of the away team
-#' @param margin The actual margin of the match, in points, from the home team perspective. Positive numbers indicate a home team win, negative numbers indicate an away team win. A value of 0 indicates a draw.
-#' @param returns The value to return as an output. Can be either the home team ELO, the away team ELO or both. Returning both will return a vector of two values with the home team ELO listed first.
-#' @param HGA Home ground advantage
-#' @return Returns the ELO of the team specified in `returns`
-#'
-#' @examples
-#' update_elo(26, elo_home = 1650, elo_away = 1500, returns = "home")
-update_elo <- function(margin, elo_home, elo_away, MOV = 1, k = 20,
-                       returns = "home", HGA = 35, M = 400, B = 0.025) {
-
-  # Error checks
-
-  # calculate elo diff
-  elo_diff <- elo_home + HGA - elo_away
-
-  # Find the expected outcome
-  expected_outcome <- find_expected_outcome(elo_diff, M = M)
-
-  # First normalises actual Outcome between 0 and 1, slightly squashed so that
-  # there are diminishing gains at higher levels.
-  actual_outcome <- map_margin_to_outcome(margin)
-
-  # Expected outcome is for home team. Away team is the negative of it, since
-  # ELO is zero sum
-  elo_change <- round((k * MOV * (actual_outcome - expected_outcome)))
-
-  # Depending on returns value
-  if (returns == "home") return(elo_home + elo_change)
-  if (returns == "away") return(elo_away - elo_change)
-  if (returns == "both") return(c(elo_home + elo_change, elo_away - elo_change))
-  if (!returns %in% c("home", "away", "both")) stop("Returns was not a valid argument. Use one of 'home', 'away' or 'both'")
-}
-
-
+#' @param results x
+#' @param HGA Home ground advantage, in arbitrary units
+#' @param M Weighting factor for ELO calculations
+#' @param stdev Standard Deviation of results
+#' @return A dataframe of simulated results
+#' 
+#' @export
 process_historical_elo <- function(results, 
                            HGA = 35, k = 20, M = 400, 
                            B = 0.025, carryover_weight = 0.6,
                            init_elo = 1500){
-  
-  # Start progress bar
-  pb <- progress_estimated(nrow(results))
-  
-  # initialise data frame
-  processed_results <- tibble()
-  
-  # Set initial ELO
+
+  # Get initial team ELO
   teams = distinct(results, Home.Team)
-  
   team_elo <- data.frame(Team = teams$Home.Team,
                          ELO = init_elo)
   
-  
-  # Step through each game
-  for (i in seq_along(results$Game)) {
-    pb$tick()$print() # update the progress bar (tick())
-    
-    # get game details
-    game <- results[i, ]
-    
-    # Get current elo
-    home_elo <- team_elo$ELO[(team_elo$Team == game$Home.Team)]
-    away_elo <- team_elo$ELO[(team_elo$Team == game$Away.Team)]
-    
-    if(game$Round.Number == 1){
-      home_elo <- calculate_season_carryover(home_elo, initial_team = init_elo, weight = carryover_weight)
-      away_elo <- calculate_season_carryover(away_elo, initial_team = init_elo, weight = carryover_weight)
-    }
-    
-    # Calculate ELO Diff
-    elo_diff <- home_elo - away_elo + HGA
-    
-    # Find expected outcome based on elo
-    exp_margin <- find_expected_margin(elo_diff, M, B = B)
-    exp_outcome <- find_expected_outcome(elo_diff, M)
-    
-    # Find MOV multiplier
-    MOV <- calculate_MOV(elo_diff, game$Margin)
-    
-    # Update ELO scores
-    new_home_elo <- update_elo(game$Margin, home_elo, away_elo, MOV = MOV, 
-               k = k, B = B, HGA = HGA, M = M) 
-    
-    new_away_elo <- update_elo(game$Margin, home_elo, away_elo, MOV = MOV, 
-                               k = k, B = B, HGA = HGA, M = M, returns = "away") 
-    
-    team_elo$ELO[(team_elo$Team == game$Home.Team)] <- new_home_elo
-    team_elo$ELO[(team_elo$Team == game$Away.Team)] <- new_away_elo
-    
-    # Add new data to df
-    game <- game %>%
-      mutate(
-        home_elo = home_elo,
-        away_elo = away_elo,
-        exp_margin = exp_margin,
-        exp_outcome = exp_outcome
-      )
-    
-    # Bind to bigger data frame
-    processed_results <- processed_results %>%
-      bind_rows(game)
-  }
+  # Run helper function, processed
+  processed <- process_matches(results, team_elo = team_elo, type = "Historical",
+                               HGA = HGA, k = k, M = M, B = B,
+                               carryover_weight = carryover_weight,
+                               init_elo = 1500)
   
   # Return
-  return(processed_results)
+  return(processed)
 }
  
+
+#' Simulate a season based on ELO
+#'
+#' \code{simulated_season} takes a fixture and simulates remaining games based upon the teams starting ELO. 
+#'
+#' INSERT DESCRIPTION
+#'
+#' @param fixture A dataframe containing upcoming matches EXPAND 
+#' @param team_elo A dataframe with each teams current ELO ratings
+#' @param simulation An optional simulation ID number
+#' @param HGA Home ground advantage, in arbitrary units
+#' @param M Weighting factor for ELO calculations
+#' @param stdev Standard Deviation of results
+#' @return A dataframe of simulated results
+#' 
+#' @importFrom magrittr %>%
+#' @import dplyr
+#' @export
+simulate_season <- function(fixture, team_elo = data.frame(), simulation = 1,
+                            stdev = 41, HGA = 35, k = 20, M = 400, 
+                            B = 0.025, carryover_weight = 0.6,
+                            init_elo = 1500){
+  
+  
+  simulated <- process_matches(results, team_elo = team_elo, type = "Simulation",
+                               HGA = HGA, k = k, M = M, B = B,
+                               stdev = stdev,
+                               carryover_weight = carryover_weight,
+                               init_elo = 1500)
+  
+  simulated_results <- simulated_results %>%
+    mutate(sim_number = simulation)
+  
+  return(simulated_results)
+}
