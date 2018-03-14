2d601d49246bd7945210301b8b48e0bcda8b9367
James Day <jamesthomasday@gmail.com>
2018-03-14 20:48:38 +1100

    adding data to global variables

--- R/FitzRoy-package.R
+++ R/FitzRoy-package.R
@@ -1,2 +1,8 @@
 #' @keywords internal
 "_PACKAGE"
+
+## quiets concerns of R CMD check re data
+if(getRversion() >= "2.15.1")  utils::globalVariables("fixture")
+if(getRversion() >= "2.15.1")  utils::globalVariables("match_results")
+if(getRversion() >= "2.15.1")  utils::globalVariables("player_stats")
+if(getRversion() >= "2.15.1")  utils::globalVariables("afldata")
