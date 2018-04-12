9f7a02cbff6b09f34879a02f9e809de47776d825
James Day <jamesthomasday@gmail.com>
2018-04-12 17:02:39 +1000

    Updating player stats

--- data-raw/player_stats/scrape_footywire_basic.R
+++ data-raw/player_stats/scrape_footywire_basic.R
@@ -3,7 +3,7 @@ library(fitzRoy)
 
 # Run function on range of id's ----
 # I've got a list of ID's that I scraped in a file called id_data.rda
-player_stats <- update_footywire_stats(check_existing = FALSE)
+player_stats <- update_footywire_stats(check_existing = TRUE)
 
 # Write data using devtools
 devtools::use_data(player_stats, overwrite = TRUE)
