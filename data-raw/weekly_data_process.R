eb833c4610e1ff4308d40246587702404af1ee59
James Day <jamesthomasday@gmail.com>
2018-04-12 16:57:27 +1000

    created weekly script

---
+++ data-raw/weekly_data_process.R
@@ -0,0 +1,20 @@
+# Weekly Script to update data
+library(here)
+
+# ID's --------------------------------------------------------------------
+source(here::here("data-raw", "Match_ids", "find_ids.R"))
+
+
+# player_stats ------------------------------------------------------------
+source(here::here("data-raw", "player_stats", "scrape_footywire_basic.R"))
+
+
+# Match results -----------------------------------------------------------
+source(here::here("data-raw", "afl_tables_match_results", "initial_match_results.R"))
+
+# weather -----------------------------------------------------------------
+source(here::here("data-raw", "weather", "BOM Data.R"))
+
+# score progression -------------------------------------------------------
+# Need this one to be update for 2018 data - currently works for 2008-2017
+# source(here::here("data-raw", "scoring_progression", "score_progression_transform.R"))
