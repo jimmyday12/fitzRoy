1e2106856314eacf23914989bd1ef4d3094c4222
James Day <jamesthomasday@gmail.com>
2018-04-19 10:56:15 +1000

    got squiggle function working

--- R/squiggle.R
+++ R/squiggle.R
@@ -1,30 +1,40 @@
 #' Title
 #'
-#' @param query 
-#' @param year 
-#' @param round 
-#' @param game 
-#' @param source 
+#' @param query
+#' @param year
+#' @param round
+#' @param game
+#' @param source
 #'
 #' @return
 #' @export
 #'
 #' @examples
-get_squiggle <- function(query = c("source", "games", "tips"),
-                     year = NULL,
-                     round = NULL,
-                     game = NULL,
-                     source = NULL){
+get_squiggle <- function(query = c("source", "games", "tips"), ...) {
   
-  # Add check for each parameter
-  
-  
-  url <- "https://api.squiggle.com.au/?"
-  
-  url <- paste0(url, "q=", query)
+  ## Add errors and warnings
+  # e.g. wrong query, wrong url, no internet
+  exp <- enexprs(...)
+
+  url <- "https://api.squiggle.com.au/?" %>%
+    paste0("q=", query)
+
+  if (length(exp > 0)) {
+    add_filt <- function(x, name) {
+      if (!is.null(x)) paste0(";", name, "=", x)
+    }
+
+    url <- exp %>%
+      purrr::map2_chr(.x = ., .y = names(.), .f = add_filt) %>%
+      purrr::reduce(paste0) %>%
+      paste0(url, .)
+  }
+
+  print(url)
   dat <- jsonlite::fromJSON(url)
-  df <- dat[[1]]
-  # Fix types
-  as.data.frame(lapply(df, function(x) type.convert(x, as.is=TRUE)), stringsAsFactors=FALSE)
   
+  # Return DF
+  df <- as.data.frame(dat[[1]])
+  df[, ] <- lapply(df[, ], as.character)
+  as.data.frame(lapply(df, function(x) type.convert(x, as.is=TRUE)), stringsAsFactors=FALSE)
 }
