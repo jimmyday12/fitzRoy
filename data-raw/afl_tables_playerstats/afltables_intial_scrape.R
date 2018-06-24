8a93294552eb116913172622e24c8db1570240c5
James Day <jamesthomasday@gmail.com>
2018-06-25 08:29:36 +1000

    setup get_tables function

--- data-raw/afl_tables_playerstats/afltables_intial_scrape.R
+++ data-raw/afl_tables_playerstats/afltables_intial_scrape.R
@@ -26,3 +26,96 @@ match %>%
 
 match_list <- read_html("https://afltables.com/afl/seas/2018.html")
 # lists"tr+ tr b+ a"
+
+# Return list of raw afltables teams
+# make fix.teams less opinionated
+# 
+
+get_afltables_ids <- function(seasons = as.integer(format(Sys.Date(), "%Y")), 
+                              teams = unique(fitzRoy::fixture$Home.Team)){
+  # Get htmls
+  seas_htmls <- 
+    seasons %>%
+    purrr::map(~ paste0("https://afltables.com/afl/seas/", ., ".html")) %>%
+    purrr::map(xml2::read_html)
+  
+  # Search for matching teams
+  home_teams <- 
+    seas_htmls %>%
+    purr::map(html_nodes, "td tr:nth-child(1) a:nth-child(1)")) %>%
+    purr::map(html_text) %>%
+    purr::map("%in%"(teams)
+  
+  away_teams <- 
+    season_htmls %>%
+    html_nodes("td tr+ tr a:nth-child(1)") %>%
+    html_text() %>%
+    "%in%"(teams)
+  
+  game_urls <- purrr::map(~ rvest::html_nodes(.,"tr+ tr b+ a")) %>%
+    purrr::map(~ rvest::html_attr(., "href")) %>%
+    purrr::reduce(c) %>%
+    purrr::map_chr(stringr::str_replace, "..", "https://afltables.com/afl")
+  
+  games_urls[home_teams | away_teams]
+  
+}
+
+get_afltables_player <- function(Years) {
+  
+  # Get XML and extract text from .data
+  game_urls <-
+    Years %>%
+    purrr::map(~ paste0("https://afltables.com/afl/seas/", ., ".html")) %>%
+    purrr::map(xml2::read_html) %>%
+    purrr::map(~ rvest::html_nodes(.,"tr+ tr b+ a")) %>%
+    purrr::map(~ rvest::html_attr(., "href")) %>%
+    purrr::reduce(c) %>%
+    purrr::map_chr(stringr::str_replace, "..", "https://afltables.com/afl")
+  
+  
+  replace_names <- function(x) {
+    names(x) <- x[1, ]
+    x[-1, ]
+  }
+  
+  # For each game url, download data, extract the stats tables #3 and #5 and bind together
+  message("Downloading data\n")
+  pb <- progress_estimated(length(game_urls))
+  
+  games_xmls <- game_urls %>%
+    purrr::map(~{
+      pb$tick()$print()
+      xml2::read_html(.)
+    })
+  message("\nFinished downloading data. Processing XML's")
+  
+  games_df <- games_xmls %>%
+    purrr::map(rvest::html_table, fill = T) %>%
+    purrr::map(magrittr::extract, c(3, 5)) %>%
+    purrr::modify_depth(1, ~ purrr::map_dfr(., replace_names)) %>%
+    purrr::reduce(dplyr::bind_rows)
+  
+  
+  # Remove columns with NA and abbreviations
+  games_df <- games_df[, !(names(games_df) %in% "NA")]
+  games_df <- games_df[, !(stringr::str_detect(names(games_df), "Abbreviations"))]
+  
+  # Fix names
+  names(games_df) <- make.names(names(games_df))
+  if("X." %in% names(games_df)) games_df <- rename(games_df, Number = X.)
+  if("X1." %in% names(games_df)) games_df <- rename(games_df, One.Percenters = X1.)
+  if("X.P" %in% names(games_df)) games_df <- rename(games_df, TOG = X.P)
+  
+  # change column types
+  games_df <- games_df %>%
+    filter(!Player %in% c("Rushed", "Totals", "Opposition"))
+  
+  games_df <- as.data.frame(lapply(games_df, function(x) type.convert(x, na.strings = "NA", as.is = TRUE)), stringsAsFactors = FALSE)
+  
+  message(paste("Returned data for", min(Years), "to", max(Years)))
+  #games_df[is.na(games_df)] <- 0
+  return(games_df)
+}
+
+
