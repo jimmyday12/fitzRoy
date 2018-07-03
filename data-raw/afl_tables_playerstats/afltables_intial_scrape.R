f21919799aa934e2db3ae8bbaf7fb71d23885821
James Day <jamesthomasday@gmail.com>
2018-07-03 23:04:31 +1000

    finished match id's function

--- data-raw/afl_tables_playerstats/afltables_intial_scrape.R
+++ data-raw/afl_tables_playerstats/afltables_intial_scrape.R
@@ -10,9 +10,14 @@ match %>%
 
 
 # details "br+ table tr:nth-child(1) td:nth-child(2)"
-match %>%
+det <- match %>%
   html_nodes("br+ table tr:nth-child(1) td:nth-child(2)") %>%
   html_text()
+
+Round <- str_extract(det, "(?<=Round:\\s)(.*)(?=\\sVenue)")
+Venue <- str_extract(det, "(?<=Venue:\\s)(.*)(?=\\Date)")
+Date <- str_extract(det, "(?<=Date:\\s)(.*)(?=\\s\\()") %>% lubridate::dmy_hm()
+Attendance <- str_extract(det, "(?<=Attendance:\\s)(.*)") %>% as.numeric()
 
 # umpires "br+ table tr:nth-child(6) td
 match %>%
@@ -28,17 +33,79 @@ match_list <- read_html("https://afltables.com/afl/seas/2018.html")
 # lists"tr+ tr b+ a"
 
 
-get_afltables_player <- function(match_ids) {
+get_afltables_match_ids <- function(start_date,
+                                    end_date = Sys.Date(),
+                                    Teams = unique(fitzRoy::match_results$Home.Team)){
   
-  # # Get XML and extract text from .data
-  # game_urls <-
-  #   Years %>%
-  #   purrr::map(~ paste0("https://afltables.com/afl/seas/", ., ".html")) %>%
-  #   purrr::map(xml2::read_html) %>%
-  #   purrr::map(~ rvest::html_nodes(.,"tr+ tr b+ a")) %>%
-  #   purrr::map(~ rvest::html_attr(., "href")) %>%
-  #   purrr::reduce(c) %>%
-  #   purrr::map_chr(stringr::str_replace, "..", "https://afltables.com/afl")
+  start_date <- lubridate::parse_date_time(start_date, c("dmy", "ymd"))
+  end_date <- lubridate::parse_date_time(end_date, c("dmy", "ymd"))
+  
+  Seasons <- format(start_date, "%Y"):format(end_date, "%Y")
+  
+  html_games <- Seasons %>%
+    purrr::map(~ paste0("https://afltables.com/afl/seas/", ., ".html")) %>%
+    purrr::map(xml2::read_html)
+  
+  bye_rounds <- html_games %>%
+    purrr::map(rvest::html_nodes, "td tr:nth-child(1) td:nth-child(2)") %>%
+    purrr::map(rvest::html_text) %>%
+    purrr::map(magrittr::is_in, "Bye")
+    
+    home_teams <- html_games %>%
+      purrr::map(rvest::html_nodes, "td tr:nth-child(1) a:nth-child(1)") %>%
+      purrr::map(rvest::html_text) %>% 
+      purrr::map(replace_teams) %>%
+      purrr::map(magrittr::is_in, Teams) %>%
+      purrr::map2(.y = bye_rounds, ~magrittr::extract(.x, !.y))
+    
+    away_teams <- html_games %>%
+      purrr::map(rvest::html_nodes, "td tr+ tr a:nth-child(1)") %>%
+      purrr::map(rvest::html_text) %>%
+      purrr::map(replace_teams) %>%
+      purrr::map(magrittr::is_in, Teams) 
+    
+    dates <- html_games %>%
+      purrr::map(rvest::html_nodes, "td tr:nth-child(1) td:nth-child(4)") %>%
+      purrr::map(rvest::html_text) %>%
+      purrr::map(stringr::str_extract, "\\d{1,2}-[A-z]{3}-\\d{4}") %>%
+      purrr::map(lubridate::dmy) %>%
+      purrr::map(~.x > start_date & .x < end_date)
+ 
+
+    match_ids <- html_games %>%
+      purrr::map(rvest::html_nodes, "tr+ tr b+ a") %>%
+      purrr::map(rvest::html_attr, "href") %>%
+      purrr::map(~stringr::str_replace(., "..", "https://afltables.com/afl")) 
+    
+    # We only want match id's where home or away team is in teams
+    ind <- home_teams %>%
+      purrr::map2(.y = away_teams, ~ .x | .y) %>%
+      purrr::map2(.y = dates, ~ .x & .y)
+    
+    # Match ID's only exist for past games, while index shows all games
+    ind <- ind %>%
+      purrr::map2(.y = match_ids, ~ magrittr::extract(.x, seq_along(.y)))
+    
+    # Return only id's that match
+    match_ids %>%
+      purrr::map2(.y = ind, ~magrittr::extract(.x, .y)) %>%
+      reduce(c)
+      
+}
+
+
+get_afltables_player <- function(match_urls) {
+  
+  # For each game url, download data, extract the stats tables #3 and #5 and bind together
+  message("Downloading data\n")
+  pb <- progress_estimated(length(match_urls))
+  
+  match_xmls <- match_urls %>%
+    purrr::map(~{
+      pb$tick()$print()
+      xml2::read_html(.)
+    })
+  message("\nFinished downloading data. Processing XMLs\n")
   
   
   replace_names <- function(x) {
@@ -46,23 +113,13 @@ get_afltables_player <- function(match_ids) {
     x[-1, ]
   }
   
-  # For each game url, download data, extract the stats tables #3 and #5 and bind together
-  message("Downloading data\n")
-  pb <- progress_estimated(length(game_urls))
-  
-  games_xmls <- game_urls %>%
-    purrr::map(~{
-      pb$tick()$print()
-      xml2::read_html(.)
-    })
-  message("\nFinished downloading data. Processing XML's")
-  
-  games_df <- games_xmls %>%
-    purrr::map(rvest::html_table, fill = T) %>%
+  games_df <- match_xmls %>%
+    purrr::map(rvest::html_table, fill = TRUE) %>%
     purrr::map(magrittr::extract, c(3, 5)) %>%
     purrr::modify_depth(1, ~ purrr::map_dfr(., replace_names)) %>%
     purrr::reduce(dplyr::bind_rows)
   
+  # Somehow get match details in this^
   
   # Remove columns with NA and abbreviations
   games_df <- games_df[, !(names(games_df) %in% "NA")]
@@ -80,61 +137,12 @@ get_afltables_player <- function(match_ids) {
   
   games_df <- as.data.frame(lapply(games_df, function(x) type.convert(x, na.strings = "NA", as.is = TRUE)), stringsAsFactors = FALSE)
   
-  message(paste("Returned data for", min(Years), "to", max(Years)))
+  #message(paste("Returned data for", min(Years), "to", max(Years)))
   #games_df[is.na(games_df)] <- 0
   return(games_df)
 }
 
 
-get_afltables_match_ids <- function(Seasons = format(Sys.Date(), "%Y"), 
-                                    Teams = unique(fitzRoy::match_results$Home.Team)){
-  
-  Seasons <- as.numeric(Seasons)
-  
-  html_game <- Seasons %>%
-    purrr::map(~ paste0("https://afltables.com/afl/seas/", ., ".html")) %>%
-    purrr::map(xml2::read_html)
-  
-  bye_rounds <- match_list %>%
-      html_nodes("td tr:nth-child(1) td:nth-child(2)") %>%
-      html_text() %>%
-      `%in%`("Bye")
-    
-    home_teams <- match_list %>%
-      html_nodes("td tr:nth-child(1) a:nth-child(1)") %>%
-      html_text() %>% 
-      replace_teams() %>%
-      `%in%`(Teams) %>%
-      `[`(!bye_rounds)
-    
-    away_teams <- match_list %>%
-      html_nodes("td tr+ tr a:nth-child(1)") %>%
-      html_text() %>%
-      replace_teams() %>%
-      `%in%`(Teams) 
-    
-    #dates <- match_list %>%
-    #  html_nodes("td tr:nth-child(1) td:nth-child(4)") %>%
-    #  html_text() %>%
-      
-      
-    match_ids <- match_list %>%
-      html_nodes("tr+ tr b+ a") %>%
-      html_attr("href") %>%
-      stringr::str_replace("..", "https://afltables.com/afl") 
-    
-    ind <- home_teams | away_teams 
-    ind <- ind[seq_along(match_ids)]
-    
-    match_ids[ind]
-    
-    #purrr::map(~ rvest::html_nodes(.,"tr+ tr b+ a")) %>%
-    #purrr::map(~ rvest::html_attr(., "href")) %>%
-    #purrr::reduce(c) %>%
-    #purrr::map_chr(stringr::str_replace, "..", "https://afltables.com/afl")
-  
-  #"td:nth-child(1) tr:nth-child(1) td:nth-child(1)"
-}
 
 match_list %>%
   html_nodes()
