173bb36dfdad9f642ec9553f86aee87083284c7f
jimmyday12 <jamesthomasday@gmail.com>
2019-02-18 11:36:41 +1100

    adding abbreviations

--- R/data.R
+++ R/data.R
@@ -6,47 +6,47 @@
 #' \describe{
 #'   \item{Date}{date of match, in yyyy-mm-dd format}
 #'   \item{Season}{season of match, in yyyy format}
-#'   \item{Round}{round of match}
-#'   \item{AF}{todo}
-#'   \item{B}{todo}
-#'   \item{BO}{todo}
-#'   \item{CCL}{todo}
-#'   \item{CG}{todo}
-#'   \item{CL}{todo}
-#'   \item{CM}{todo}
-#'   \item{CP}{todo}
-#'   \item{D}{todo}
-#'   \item{DE}{todo}
-#'   \item{ED}{todo}
-#'   \item{FA}{todo}
-#'   \item{FF}{todo}
-#'   \item{G}{todo}
-#'   \item{GA}{todo}
-#'   \item{GA1}{todo}
-#'   \item{HB}{todo}
-#'   \item{HO}{todo}
-#'   \item{I50}{todo}
-#'   \item{ITC}{todo}
-#'   \item{K}{todo}
-#'   \item{M}{todo}
-#'   \item{MG}{todo}
-#'   \item{MI5}{todo}
-#'   \item{Match_id}{todo}
-#'   \item{One.Percenters}{todo}
-#'   \item{Opposition}{todo}
-#'   \item{Player}{todo}
-#'   \item{R50}{todo}
-#'   \item{SC}{todo}
-#'   \item{SCL}{todo}
-#'   \item{SI}{todo}
+#'   \item{Round}{round of season}
+#'   \item{AF}{AFL Fantasy points}
+#'   \item{B}{Behinds}
+#'   \item{BO}{Bounces}
+#'   \item{CCL}{Centre clearances}
+#'   \item{CG}{Clangers}
+#'   \item{CL}{Clearances}
+#'   \item{CM}{Contested marks}
+#'   \item{CP}{Contested possessions}
+#'   \item{D}{Disposals}
+#'   \item{DE}{Disposal efficiency, in %}
+#'   \item{ED}{Effective disposals}
+#'   \item{FA}{Free kicks against}
+#'   \item{FF}{Free kicks for}
+#'   \item{G}{Goals}
+#'   \item{GA}{Goal assists}
+#'   \item{GA1}{Goal assists on advanced page}
+#'   \item{HB}{Handballs}
+#'   \item{HO}{Hitouts}
+#'   \item{I50}{Inside 50's}
+#'   \item{ITC}{Intercepts}
+#'   \item{K}{Kicks}
+#'   \item{M}{Marks}
+#'   \item{MG}{Metres gained}
+#'   \item{MI5}{Marks inside 50}
+#'   \item{Match_id}{Match ID from Footywire}
+#'   \item{One.Percenters}{One percenters}
+#'   \item{Opposition}{Opposition team}
+#'   \item{Player}{Player name}
+#'   \item{R50}{Rebaound 50's}
+#'   \item{SC}{Super coach points}
+#'   \item{SCL}{Stoppage clearances}
+#'   \item{SI}{Score involvements}
 #'   \item{Status}{todo}
-#'   \item{T}{todo}
-#'   \item{T5}{todo}
-#'   \item{TO}{todo}
-#'   \item{TOG}{todo}
-#'   \item{Team}{todo}
-#'   \item{UP}{todo}
-#'   \item{Venue}{todo}
+#'   \item{T}{Tackles}
+#'   \item{T5}{Tackles inside 50}
+#'   \item{TO}{Turnovers}
+#'   \item{TOG}{Time on ground}
+#'   \item{Team}{Team playing for}
+#'   \item{UP}{Uncontensted possessions}
+#'   \item{Venue}{Venue match played at}
 #'   ...
 #' }
 #' @source \url{http://www.footywire.com}
@@ -75,22 +75,22 @@
 #'
 #' @format A data frame with 15200 rows and 16 variables:
 #' \describe{
-#'   \item{Game}{todo}
-#'   \item{Date}{todo}
-#'   \item{Round}{todo}
-#'   \item{Home.Team}{todo}
-#'   \item{Home.Goals}{todo}
-#'   \item{Home.Behinds}{todo}
-#'   \item{Home.Points}{todo}
-#'   \item{Away.Team}{todo}
-#'   \item{Away.Goals}{todo}
-#'   \item{Away.Behinds}{todo}
-#'   \item{Away.Points}{todo}
-#'   \item{Venue}{todo}
-#'   \item{Margin}{todo}
-#'   \item{Season}{todo}
-#'   \item{Round.Type}{todo}
-#'   \item{Round.Number}{todo}
+#'   \item{Game}{Historical match count}
+#'   \item{Date}{Date of match}
+#'   \item{Round}{Round of season}
+#'   \item{Home.Team}{Home team name}
+#'   \item{Home.Goals}{Home team goals scored}
+#'   \item{Home.Behinds}{Home team behinds scored}
+#'   \item{Home.Points}{Home team points scored}
+#'   \item{Away.Team}{Away team name}
+#'   \item{Away.Goals}{Away team goals scored}
+#'   \item{Away.Behinds}{Away team behinds scored}
+#'   \item{Away.Points}{Away team points scored}
+#'   \item{Venue}{Venue match played out}
+#'   \item{Margin}{Final margin in favour of home team}
+#'   \item{Season}{Season year}
+#'   \item{Round.Type}{Round type; either regular or finals}
+#'   \item{Round.Number}{Round number in numeric format}
 #'
 #' }
 #' @source \url{http://www.afltables.com}
