b74ce38e6df0b827ab8167586afec7282a541485
jimmyday12 <jamesthomasday@gmail.com>
2020-01-21 09:08:20 +1100

    adding more data for vignettes

--- data-raw/vignette_data.R
+++ data-raw/vignette_data.R
@@ -1,3 +1,6 @@
+# This script gets data that is going to be used by the Vignettes. Add data to here where required. 
+
+
 # elo-ratings script to get data for vignette
 devtools::install_github("jimmyday12/fitzRoy")
 library(dplyr)
@@ -13,14 +16,16 @@ fixture <- fitzRoy::get_fixture(2019)
 #results <- get_match_results()
 stats <- get_afltables_stats(start_date = "2018-01-01", end_date = "2018-06-01")
 #fixture <- get_fixture()
-stats_9927 <- get_footywire_stats(ids = 9927)
+stats_gf <- get_footywire_stats(ids = 9927)
 ladder <- return_ladder(match_results_df = results)
-ladder_2018_15 <- return_ladder(match_results_df = results, season_round = 15, season = 2018)
-
+ladder_round <- return_ladder(match_results_df = results, season_round = 15, season = 2018)
+sources <- get_squiggle_data("sources")
+tips <- get_squiggle_data("tips")
+tips_round <- get_squiggle_data("tips", round = 1, year = 2018)
 
 # womens-stats
 cookie <- get_aflw_cookie()
 
 
-usethis::use_data(results, fixture, stats, stats_9927, ladder, ladder_2018_15,
+usethis::use_data(results, fixture, stats, stats_gf, ladder, ladder_round, sources, tips, tips_round,
                   internal = TRUE, overwrite = TRUE)
