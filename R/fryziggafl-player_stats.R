e13f8d01e4c7369bbe425a32b6fc7aec45674144
Matt <matthewjmoyle@outlook.com>
2020-05-27 13:28:07 +0800

    Added caching mechanism to speed up code. Offline = TRUE is default behaviour which uses cached results, offline=FALSE updates the cache and returns result.

--- R/fryziggafl-player_stats.R
+++ R/fryziggafl-player_stats.R
@@ -9,6 +9,7 @@
 #'
 #' @param start optional, character string or numeric for start year, in "YYYY"ormat
 #' @param end optional, character string or numeric for end year, in "YYYY"format
+#' @param offline optional, boolean, TRUE will use offline cached data, FALSE returns live data and updates the cache.
 #'
 #' @return a data table containing player stats for each game between start and end years
 #' @export
@@ -25,41 +26,50 @@
 #' @importFrom rlang .data
 
 get_fryzigg_stats <- function(start = 1897,
-                              end = as.numeric(format(Sys.Date(), "%Y"))) {
-  source("R/helper.R")
-  OFFSET <- 100000 #this is the max as per rate limit
+                              end = as.numeric(format(Sys.Date(), "%Y")),
+                              offline = TRUE) {
   start <- verify_year(start)
   end <- verify_year(end)
+  if(!offline){
+    OFFSET <- 100000 #this is the max as per rate limit
+    first_year <- verify_year(1897)
+    last_year <- verify_year(as.numeric(format(Sys.Date(), "%Y")))
+    message(paste("Updating cached data from", first_year, "to", last_year, "\n ", "This may take some time."))
+    BASE <- paste0("http://www.fryziggafl.net/api/matches/all/?format=json&start=", first_year,
+                   "&end=", last_year,
+                   "&limit=", format(OFFSET, scientific = F),
+                   "&offset=")
 
-  message(paste("Returning data from", start, "to", end, "\n ", "This may take some time."))
-  BASE <- paste0("http://www.fryziggafl.net/api/matches/all/?format=json&start=", start,
-                 "&end=", end,
-                 "&limit=", format(OFFSET, scientific = F),
-                 "&offset=")
+    response <- httr::GET(paste0(BASE, 0))
+    parsed <- jsonlite::fromJSON(httr::content(response, as = "text", encoding = "UTF-8"), flatten = TRUE)
+    count <- unlist(parsed['count'])
+    pages <- floor(count / OFFSET)
+    stats_df <- as.data.frame(parsed['results'])
 
-  response <- httr::GET(paste0(BASE, 0))
-  parsed <- jsonlite::fromJSON(httr::content(response, as = "text", encoding = "UTF-8"), flatten = TRUE)
-  count <- unlist(parsed['count'])
-  pages <- floor(count / OFFSET)
-  stats_df <- as.data.frame(parsed['results'])
+    if (pages > 1) {
+      for (page in 1:pages) {
+        message(paste("Pulling Page", page, "of", pages))
+        current_offset <- page * OFFSET
 
-  if (pages > 1) {
-    for (page in 1:pages) {
-      message(paste("Pulling Page", page, "of", pages))
-      current_offset <- page * OFFSET
+        call <- paste0(BASE, format(current_offset, scientific = F))
+        response <- httr::GET(call)
+        parsed <- jsonlite::fromJSON(httr::content(response, as = "text", encoding = "UTF-8"), flatten = TRUE)
 
-      call <- paste0(BASE, format(current_offset, scientific = F))
-      response <- httr::GET(call)
-      parsed <- jsonlite::fromJSON(httr::content(response, as = "text", encoding = "UTF-8"), flatten = TRUE)
-
-      tmp <- as.data.frame(parsed['results'])
-      stats_df <- rbind(stats_df, tmp)
+        tmp <- as.data.frame(parsed['results'])
+        stats_df <- rbind(stats_df, tmp)
+      }
     }
+    stats_df <- stats_df %>%
+      dplyr::rename_all(~stringr::str_replace_all(., "results\\.", "")) #clean column names
+    stats_df <- stats_df[, c(80, 57:79, 1:56)]  #hard-coded but will need to be ammended.
+    #reorder columns
+    saveRDS(stats_df, file = paste0(here::here(), "\\data-raw\\fryzigg-api\\fryziggafl.rds"))
   }
-  stats_df <- stats_df %>%
-    dplyr::rename_all(~stringr::str_replace_all(., "results\\.", "")) #clean column names
-  stats_df <- stats_df[, c(80, 57:79, 1:56)]  #hard-coded but will need to be ammended.
-  #reorder columns
+  message(paste("Returning cached data from", start, "to", end, "\n ", "This may take some time."))
+  stats_df <- readRDS(file = paste0(here::here(), "\\data-raw\\fryzigg-api\\fryziggafl.rds"))
+  stats_df <- subset(stats_df, format(as.Date(stats_df$match_date),"%Y") >= start &
+    format(as.Date(stats_df$match_date),"%Y") <= end)
   return(stats_df)
 }
 
+#get_fryzigg_stats(offline = FALSE)
