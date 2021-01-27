2e917e6cb18b1c810530a765c81973b077c58738
jimmyday12 <jamesday@JDs-Macbook-Pro.brisbane.fusion.sport>
2021-01-27 14:59:45 +1100

    supressingwarnings

--- tests/testthat/test-helpers-footywire.R
+++ tests/testthat/test-helpers-footywire.R
@@ -14,11 +14,12 @@ test_that("get_footywire_stats work with different inputs", {
   testthat::skip_if_offline()
   testthat::skip_on_cran()
   
-  expect_type(get_footywire_stats(5000), "list")
-  expect_error(get_footywire_stats(1))
-  expect_error(get_footywire_stats("a"))
-  expect_error(get_footywire_stats())
-  expect_error(get_footywire_stats(1:2))
+  expect_warning(dat <- get_footywire_stats(5000))
+  expect_type(dat, "list")
+  expect_error(supressWarnings(get_footywire_stats(1)))
+  expect_error(supressWarnings(get_footywire_stats("a")))
+  expect_error(supressWarnings(get_footywire_stats()))
+  expect_error(supressWarnings(get_footywire_stats(1:2)))
 })
 
 
