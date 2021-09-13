7acaf13ac5fb28099bd1fdd2c48d339c4f893e7b
jimmyday12 <jamesday@JDs-Macbook-Pro.brisbane.fusion.sport>
2021-09-13 13:45:20 +1000

    merging remote changes

--- R/helpers-afl.R
+++ R/helpers-afl.R
@@ -15,9 +15,7 @@ find_team_id <- function(team_abr, comp = "AFLM") {
   resp <- httr::GET(url = api,
                     query = list("pageSize" = "1000"))
   
-  cont <- resp %>%
-    httr::content(as = "text", encoding = "UTF-8") %>%
-    jsonlite::fromJSON(flatten = TRUE)
+  cont <- parse_resp_afl(resp)
   
   df <- cont$teams %>%
     na.omit() %>%
@@ -91,20 +89,19 @@ team_abr_afl <- function(team) {
 #' @keywords internal
 #' @noRd
 find_comp_id <- function(comp) {
-  if (!comp %in% c("AFLM", "AFLW")) rlang::abort(glue::glue("Comp should be either \"AFLW\" or \"AFL\"
-                                                 You supplied {comp}"))
+  
+  comp <- check_comp(comp)
 
-  api_url <- paste0("https://aflapi.afl.com.au/afl/v2/competitions/")
+  api_url <- httr::modify_url("https://aflapi.afl.com.au",
+                              path = "/afl/v2/competitions/")
 
-  comps_dat <- httr::GET(api_url)
-
-  comps_cont <- comps_dat %>%
-    httr::content(as = "text", encoding = "UTF-8") %>%
-    jsonlite::fromJSON(flatten = TRUE)
-
+  resp <- httr::GET(api_url)
+  
+  cont <- parse_resp_afl(resp)
+  
   if (comp == "AFLM") comp <- "AFL"
 
-  ids <- comps_cont$competitions$id[comps_cont$competitions$code == comp]
+  ids <- cont$competitions$id[cont$competitions$code == comp]
   min(ids, na.rm = TRUE)
 }
 
@@ -136,23 +133,18 @@ get_afl_cookie <- function() {
 find_season_id <- function(season, comp = "AFLM") {
   # check inputs
   season <- check_season(season)
-  check_comp(comp)
+  comp <- check_comp(comp)
 
   comp_id <- find_comp_id(comp)
 
-  compSeasons_url <- paste0(
-    "https://aflapi.afl.com.au/afl/v2/competitions/",
-    comp_id,
-    "/compseasons"
-  )
+  api <- httr::modify_url("https://aflapi.afl.com.au",
+                          path = paste0("/afl/v2/competitions/", comp_id, "/compseasons"))
 
-  comp_dat <- httr::GET(compSeasons_url)
+  resp <- httr::GET(api)
+  
+  cont <- parse_resp_afl(resp)
 
-  comp_cont <- comp_dat %>%
-    httr::content(as = "text", encoding = "UTF-8") %>%
-    jsonlite::fromJSON(flatten = TRUE)
-
-  comp_ids <- comp_cont$compSeasons %>%
+  comp_ids <- cont$compSeasons %>%
     dplyr::mutate(season = as.numeric(gsub("([0-9]+).*$", "\\1", .data$name)))
 
   id <- comp_ids$id[match(season, comp_ids$season)]
@@ -173,8 +165,11 @@ find_season_id <- function(season, comp = "AFLM") {
 #' @param season Season, in YYYY format.
 #' @keywords internal
 #' @noRd
-find_round_id <- function(round_number, season = NULL, season_id = NULL, 
-                          comp = "AFLM", providerId = FALSE, future_rounds = TRUE) {
+find_round_id <- function(round_number, season = NULL, 
+                          season_id = NULL, 
+                          comp = "AFLM", 
+                          providerId = FALSE, 
+                          future_rounds = TRUE) {
 
   if (providerId) {
     id_name <- "providerId"
@@ -184,22 +179,21 @@ find_round_id <- function(round_number, season = NULL, season_id = NULL,
   
   # check inputs
   season <- check_season(season)
-  check_comp(comp)
+  comp <- check_comp(comp)
 
   if (is.null(season_id)) season_id <- find_season_id(season, comp)
 
-  round_url <-  paste0(
-    "https://aflapi.afl.com.au/afl/v2/compseasons/",
-    season_id,
-    "/rounds")
+  api <-  httr::modify_url("https://aflapi.afl.com.au",
+                                 path = paste0("/afl/v2/compseasons/",
+                                               season_id,
+                                               "/rounds"))
 
-  round_dat <- httr::GET(round_url, query = list(pageSize = 30))
+  resp <- httr::GET(api, 
+                    query = list(pageSize = 30))
+  
+  cont <- parse_resp_afl(resp)
 
-  round_cont <- round_dat %>%
-    httr::content(as = "text") %>%
-    jsonlite::fromJSON(flatten = TRUE)
-
-  df <- round_cont$rounds
+  df <- cont$rounds
   
   if (!future_rounds) {
     df <- df[df$utcStartTime < Sys.Date() & df$utcStartTime != "", ]
@@ -213,7 +207,8 @@ find_round_id <- function(round_number, season = NULL, season_id = NULL,
   
 
   if (length(id) < 1) {
-    rlang::abort(glue::glue("No data found for specified round number and season. Does round number \"{round_number}\" exist for Season \"{season}\" on \"www.afl.com.au\"?"))
+    rlang::warn(glue::glue("No data found for specified round number and season"))
+    return(NULL)
   }
   return(id)
 }
@@ -227,8 +222,9 @@ find_round_id <- function(round_number, season = NULL, season_id = NULL,
 #' @noRd
 fetch_match_roster_afl <- function(id, cookie = NULL) {
   if (is.null(cookie)) cookie <- get_afl_cookie()
-  #print(id)
-  api <- paste0("https://api.afl.com.au/cfs/afl/matchRoster/full/", id)
+  
+  api <- httr::modify_url("https://api.afl.com.au",
+                          path = paste0("/cfs/afl/matchRoster/full/", id))
   
   resp <- httr::GET(
     url = api,
@@ -237,14 +233,13 @@ fetch_match_roster_afl <- function(id, cookie = NULL) {
     )
   )
   
-  cont <- resp %>%
-    httr::content(as = "text", encoding = "UTF-8") %>%
-    jsonlite::fromJSON(flatten = TRUE)
+  cont <- parse_resp_afl(resp)
   
   cont$matchRoster$homeTeam$clubDebuts <- list()
   cont$matchRoster$homeTeam$milestones <- list()
   cont$matchRoster$homeTeam$ins <- list()
   cont$matchRoster$homeTeam$outs <- list()
+  
   home_df <- cont$matchRoster$homeTeam %>%
     purrr::compact() %>%
     purrr::flatten_dfr() %>%
@@ -254,6 +249,7 @@ fetch_match_roster_afl <- function(id, cookie = NULL) {
   cont$matchRoster$awayTeam$milestones <- list()
   cont$matchRoster$awayTeam$ins <- list()
   cont$matchRoster$awayTeam$outs <- list()
+  
   away_df <- cont$matchRoster$awayTeam %>%
     purrr::compact() %>%
     purrr::flatten_dfr() %>%
@@ -272,10 +268,10 @@ fetch_match_roster_afl <- function(id, cookie = NULL) {
 fetch_match_stats_afl <- function(id, cookie = NULL) {
   
   if (is.null(cookie)) cookie <- get_aflw_cookie()
-  #print(id)
-  # Make request
   
-  api <- paste0("https://", "api.afl.com.au", "/cfs/afl/playerStats/match/", id)
+  api <- httr::modify_url(url = "https://api.afl.com.au",
+                          path = paste0("/cfs/afl/playerStats/match/", id))
+
   resp <- httr::GET(
     url = api,
     httr::add_headers(
@@ -283,9 +279,7 @@ fetch_match_stats_afl <- function(id, cookie = NULL) {
     )
   )
   
-  cont <- resp %>%
-    httr::content(as = "text", encoding = "UTF-8") %>%
-    jsonlite::fromJSON(flatten = TRUE)
+  cont <- parse_resp_afl(resp)
   
   home_df <- cont$homeTeamPlayerStats %>%
     dplyr::select(-.data$teamId, -.data$playerStats.lastUpdated) %>%
@@ -330,15 +324,14 @@ clean_names_playerstats_afl <- function(x){
 fetch_round_results_afl <- function(id, cookie = NULL){
 
   if (is.null(cookie)) cookie <- get_afl_cookie()
-  # make call
-  url_api <- paste0("http://api.afl.com.au/cfs/afl/matchItems/round/", id)
-  resp <- httr::GET(url_api,
-                    httr::add_headers(`X-media-mis-token` = cookie))
   
-  # extract json
-  cont <- resp %>%
-    httr::content(as = "text", encoding = "UTF-8") %>%
-    jsonlite::fromJSON(flatten = TRUE)
+  url_api <- httr::modify_url("http://api.afl.com.au",
+                             path =  paste0("/cfs/afl/matchItems/round/", id))
+  
+  resp <- httr::GET(url_api,
+                    httr::add_headers("x-media-mis-token" = cookie))
+  
+  cont <- parse_resp_afl(resp)
   
   df <- dplyr::as_tibble(cont$items)
   
@@ -355,7 +348,7 @@ fetch_round_results_afl <- function(id, cookie = NULL){
   
   return(df)
 }
-  
+
 
 #' Fetches squad 
 #'
@@ -367,6 +360,7 @@ fetch_squad_afl <- function(teamId, team, compSeasonId) {
   
   cli_team <- cli::cli_process_start("Fetching player details for {team}")
   api <- "https://aflapi.afl.com.au//afl/v2/squads"
+  
   resp <- httr::GET(
     url = api,
     query = list(
@@ -376,9 +370,7 @@ fetch_squad_afl <- function(teamId, team, compSeasonId) {
     )
   )
   
-  cont <- resp %>%
-    httr::content(as = "text", encoding = "UTF-8") %>%
-    jsonlite::fromJSON(flatten = TRUE)
+  cont <- parse_resp_afl(resp)
   
   df <- dplyr::as_tibble(cont$squad$players)
   
@@ -389,5 +381,28 @@ fetch_squad_afl <- function(teamId, team, compSeasonId) {
   df %>%
     dplyr::select(firstName, surname, id, everything())
 }
+
+#' Parses afl response and checks for errors
+#'
+#' @param resp response object returned from POST/GET
+#' @keywords internal
+#' @noRd  
+parse_resp_afl <- function(resp) {
+  if (httr::http_type(resp) != "application/json") {
+    rlang::abort("API did not return json", call. = FALSE)
+  }
   
+  parsed <- resp %>% 
+    httr::content(as = "text", encoding = "UTF-8") %>%
+    jsonlite::fromJSON(flatten = TRUE)
+  
+  if (httr::http_error(resp)) {
+    rlang::abort(glue::glue(
+      "GitHub API request failed 
+      {httr::status_code(resp)} - {parsed$techMessage}"),
+      call. = FALSE
+    )
+  }
+  return(parsed)
+}
   
