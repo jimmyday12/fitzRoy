363e00f5dba2293dfbc86f3b245d95989aeb24b8
James Day <jamesthomasday@gmail.com>
2023-02-28 21:53:48 +1100

    adding lifecycle support

--- R/FitzRoy-package.R
+++ R/FitzRoy-package.R
@@ -6,3 +6,8 @@
 ## quiets concerns of R CMD check re data
 if (getRversion() >= "2.15.1") utils::globalVariables("player_stats")
 if (getRversion() >= "2.15.1") utils::globalVariables(c("."))
+
+## usethis namespace: start
+#' @importFrom lifecycle deprecated
+## usethis namespace: end
+NULL
