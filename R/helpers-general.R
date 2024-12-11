5d41eb1f3e7ef0ea44ee73f2c7870f25ea05bbf3
peteowen1 <pete.owen1@hotmail.co.uk>
2024-12-11 15:43:21 +1100

    documentation for 1.6

--- R/helpers-general.R
+++ R/helpers-general.R
@@ -176,7 +176,7 @@ return_start_end_dates <- function(season) {
 parse_fitzroy_match_id <- function(df, source = "AFL") {
   source <- check_source(source)
 
-  round_mapping <- round_mapping
+  # round_mapping <- round_mapping (build a round mapping)
 
   if (source == "AFL") {
     df <- df %>%
