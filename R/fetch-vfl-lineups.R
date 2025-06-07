8e672aeaf4168e179f81ac4ba36cd25d849423aa
DanielTomaro <danieltomaro@icloud.com>
2025-06-07 11:37:06 +1000

    Add fetch_vfl_lineups to scrape upcoming team lineups with guernsey numbers

---
+++ R/fetch-vfl-lineups.R
@@ -0,0 +1,45 @@
+#' Fetch upcoming VFL team lineups
+#'
+#' @return A tibble of upcoming VFL team lineups with guernsey numbers
+#' @export
+#'
+#' @examples
+#' fetch_vfl_lineups()
+fetch_vfl_lineups <- function() {
+  url <- "https://vfl.aflmstats.com/lineups"
+  
+  page <- tryCatch({
+    rvest::read_html(url)
+  }, error = function(e) {
+    stop("Failed to retrieve VFL lineups. Check the URL or your internet connection.")
+  })
+  
+  # Extract team names
+  team_names <- page %>%
+    rvest::html_elements("table tr:nth-child(1) th") %>%
+    rvest::html_text(trim = TRUE)
+  
+  # Extract all ol (ordered lists) for each team
+  team_lineups <- page %>%
+    rvest::html_elements("table tr:nth-child(2) td ol")
+  
+  # Map each team to its list of players
+  all_lineups <- purrr::map2_dfr(
+    team_names,
+    team_lineups,
+    function(team, ol_node) {
+      players <- ol_node %>%
+        rvest::html_elements("li")
+      
+      purrr::map_dfr(players, function(player_node) {
+        tibble::tibble(
+          Team = team,
+          Guernsey = as.integer(player_node %>% rvest::html_attr("value")),
+          Player = player_node %>% rvest::html_text(trim = TRUE)
+        )
+      })
+    }
+  )
+  
+  return(all_lineups)
+}
