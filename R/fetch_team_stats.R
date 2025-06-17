3ec631e4add4feff47666fbef863188e7b0cedf3
DanielTomaro <danieltomaro@icloud.com>
2025-06-17 20:22:43 +1000

    Draft - Adding footywire

--- R/fetch_team_stats.R
+++ R/fetch_team_stats.R
@@ -3,22 +3,28 @@
 #' General wrapper for fetching team statistics from a specified source.
 #'
 #' @param season Integer. The season to fetch stats for (e.g. 2024).
-#' @param summary_type Character. Either `"totals"` (default) or `"averages"`.
-#' @param source Character. Currently only `"afltables"` is supported.
+#' @param summary_type Character. Either `"totals"` (default), `"averages"`, or other type depending on source.
+#'                     If `source = "footywire"` and `summary_type = NULL`, returns all types.
+#' @param source Character. One of `"afltables"` or `"footywire"`.
 #' @param ... Additional arguments passed to the underlying data source function.
 #'
 #' @return A data frame of team stats for the season.
 #' @export
 #'
 #' @examples
-#' fetch_team_stats(2023)
-#' fetch_team_stats(2024, summary_type = "averages")
+#' fetch_team_stats(2024)
+#' fetch_team_stats(2023, summary_type = "averages", source = "footywire")
 fetch_team_stats <- function(season,
                              summary_type = "totals",
-                             source = "afltables",
+                             source = c("afltables", "footywire"),
                              ...) {
+  source <- match.arg(source)
+  
   dat <- switch(source,
-                "afltables" = fetch_team_stats_afltables(season, summary_type),
+                "afltables" = fetch_team_stats_afltables(season = season, summary_type = summary_type),
+                "footywire" = fetch_team_stats_footywire(season = season,
+                                                         summary_type = if (identical(summary_type, "totals")) "totals" else summary_type,
+                                                         ...),
                 NULL
   )
   
@@ -28,7 +34,6 @@ fetch_team_stats <- function(season,
   
   return(dat)
 }
-
 
 #' Fetch Team Statistics from AFLTables
 #'
@@ -127,4 +132,88 @@ fetch_team_stats_afltables <- function(season, summary_type = "totals") {
   }
   
   return(team_stats_final)
+}
+
+#' Fetch Team Statistics from Footywire
+#'
+#' @description
+#' Scrapes team-level statistics (averages, totals, opponent stats) from Footywire for a given season.
+#'
+#' @param season Integer. A year between 2003 and the current year.
+#' @param summary_type Character. One of "totals", "averages", "opp_totals", "opp_averages", "diff_totals", "diff_averages".
+#'                     If NULL (default), all types will be fetched and combined.
+#'
+#' @return A data frame of team stats, including team name, games, and all statistical columns.
+#' @export
+#'
+#' @examples
+#' fetch_team_stats_footywire(2024, summary_type = "totals")
+#' fetch_team_stats_footywire(2023)
+fetch_team_stats_footywire <- function(season,
+                                       summary_type = c("totals", "averages",
+                                                        "opp_totals", "opp_averages",
+                                                        "diff_totals", "diff_averages")) {
+  summary_type <- match.arg(summary_type,
+                            choices = c("totals", "averages", "opp_totals", "opp_averages", "diff_totals", "diff_averages"),
+                            several.ok = TRUE)
+  
+  type_map <- c(
+    totals = "TT",
+    averages = "TA",
+    opp_totals = "OT",
+    opp_averages = "OA",
+    diff_totals = "DT",
+    diff_averages = "DA"
+  )
+  
+  fetch_single_type <- function(season, type) {
+    type_code <- type_map[type]
+    url <- glue::glue("https://www.footywire.com/afl/footy/ft_team_rankings?year={season}&type={type_code}&sby=2")
+    
+    page <- tryCatch(
+      rvest::read_html(url),
+      error = function(e) rlang::abort(glue::glue("Could not read page: <{url}>"))
+    )
+    
+    tables <- page %>% rvest::html_elements("table")
+    if (length(tables) < 11) rlang::abort("Expected data table not found on page.")
+    
+    rows <- tables[[11]] %>% rvest::html_elements("tr") %>% .[-1]
+    
+    parsed <- purrr::map_dfr(rows, function(row) {
+      cols <- row %>% rvest::html_elements("td")
+      if (length(cols) < 20) return(NULL)
+      
+      tibble::tibble(
+        Team   = cols[[2]] %>% rvest::html_element("a") %>% rvest::html_text(trim = TRUE),
+        Games  = cols[[3]] %>% rvest::html_text(trim = TRUE) %>% readr::parse_number(),
+        K      = cols[[4]] %>% rvest::html_text(trim = TRUE) %>% readr::parse_number(),
+        HB     = cols[[5]] %>% rvest::html_text(trim = TRUE) %>% readr::parse_number(),
+        D      = cols[[6]] %>% rvest::html_text(trim = TRUE) %>% readr::parse_number(),
+        M      = cols[[7]] %>% rvest::html_text(trim = TRUE) %>% readr::parse_number(),
+        G      = cols[[8]] %>% rvest::html_text(trim = TRUE) %>% readr::parse_number(),
+        GA     = cols[[9]] %>% rvest::html_text(trim = TRUE) %>% readr::parse_number(),
+        I50    = cols[[10]] %>% rvest::html_text(trim = TRUE) %>% readr::parse_number(),
+        BH     = cols[[11]] %>% rvest::html_text(trim = TRUE) %>% readr::parse_number(),
+        T      = cols[[12]] %>% rvest::html_text(trim = TRUE) %>% readr::parse_number(),
+        HO     = cols[[13]] %>% rvest::html_text(trim = TRUE) %>% readr::parse_number(),
+        FF     = cols[[14]] %>% rvest::html_text(trim = TRUE) %>% readr::parse_number(),
+        FA     = cols[[15]] %>% rvest::html_text(trim = TRUE) %>% readr::parse_number(),
+        CL     = cols[[16]] %>% rvest::html_text(trim = TRUE) %>% readr::parse_number(),
+        CG     = cols[[17]] %>% rvest::html_text(trim = TRUE) %>% readr::parse_number(),
+        R50    = cols[[18]] %>% rvest::html_text(trim = TRUE) %>% readr::parse_number(),
+        AF     = cols[[19]] %>% rvest::html_text(trim = TRUE) %>% readr::parse_number(),
+        SC     = cols[[20]] %>% rvest::html_text(trim = TRUE) %>% readr::parse_number()
+      )
+    })
+    
+    parsed %>%
+      dplyr::mutate(
+        Season = !!season,
+        Source = !!type
+      ) %>%
+      dplyr::relocate(dplyr::all_of(c("Season", "Team", "Games", "Source")))
+  }
+  
+  purrr::map_dfr(summary_type, ~ fetch_single_type(season, .x))
 }
